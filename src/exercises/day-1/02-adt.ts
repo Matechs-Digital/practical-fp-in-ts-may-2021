bc7e2e36a4c59a750424d4d247aa273541715c99
Michael Arnaldi <arnaldimichael@gmail.com>
2021-05-04 15:22:35 +0100

    cleaup

--- src/exercises/day-1/02-adt.ts
+++ src/exercises/day-1/02-adt.ts
@@ -7,8 +7,6 @@
  * your day-to-day problems.
  */
 
-import { pipe } from "@effect-ts/system/Function"
-
 /**
  * Segment:
  *
@@ -44,94 +42,27 @@ export declare const falseValue: BooleanADT
  * - Mul (describe a multiplication operation of 2 expressions)
  * - Div (describe a division operation of 2 expressions)
  */
-export type MathExpr = Value | Add | Sub | Mul | Div
-
-export class Value {
-  readonly _tag = "Value"
-  constructor(readonly value: number) {}
-}
-
-export class Add {
-  readonly _tag = "Add"
-  constructor(readonly x: MathExpr, readonly y: MathExpr) {}
-}
-
-export class Sub {
-  readonly _tag = "Sub"
-  constructor(readonly x: MathExpr, readonly y: MathExpr) {}
-}
-
-export class Mul {
-  readonly _tag = "Mul"
-  constructor(readonly x: MathExpr, readonly y: MathExpr) {}
-}
-
-export class Div {
-  readonly _tag = "Div"
-  constructor(readonly x: MathExpr, readonly y: MathExpr) {}
-}
+export type MathExpr = never
 
 /**
  * Exercise:
  *
  * Create constructors for all the members in MathExpr (pipeable)
  */
-export function value(value: number): MathExpr {
-  return new Value(value)
-}
-
-export function add(y: MathExpr): (x: MathExpr) => MathExpr {
-  return (x) => new Add(x, y)
-}
-
-export function sub(y: MathExpr): (x: MathExpr) => MathExpr {
-  return (x) => new Sub(x, y)
-}
-
-export function mul(y: MathExpr): (x: MathExpr) => MathExpr {
-  return (x) => new Mul(x, y)
-}
-
-export function div(y: MathExpr): (x: MathExpr) => MathExpr {
-  return (x) => new Div(x, y)
-}
 
 /**
  * Exercise:
  *
  * Create a small program using the MathExpr constructors
  */
-export const program = pipe(
-  value(0),
-  add(pipe(value(1), mul(value(10)))),
-  add(value(2)),
-  mul(value(3))
-)
+export declare const program: MathExpr
 
 /**
  * Exercise:
  *
  * Implement the function evaluate
  */
-export function evaluate(expr: MathExpr): number {
-  switch (expr._tag) {
-    case "Value": {
-      return expr.value
-    }
-    case "Add": {
-      return evaluate(expr.x) + evaluate(expr.y)
-    }
-    case "Mul": {
-      return evaluate(expr.x) * evaluate(expr.y)
-    }
-    case "Sub": {
-      return evaluate(expr.x) - evaluate(expr.y)
-    }
-    case "Div": {
-      return evaluate(expr.x) / evaluate(expr.y)
-    }
-  }
-}
+export declare function evaluate(expr: MathExpr): number
 
 /**
  * Exercise:
--- src/exercises/day-1/03-gadt.ts
+++ src/exercises/day-1/03-gadt.ts
@@ -6,8 +6,6 @@
  * In this module we introduce a generalization to algebraic data types required to deal with generic patameters.
  */
 
-import { identity } from "@effect-ts/system/Function"
-
 /**
  * Segment:
  *
@@ -20,68 +18,7 @@ import { identity } from "@effect-ts/system/Function"
  * We would like to port over the previous module Expr<number> to a more general Expr<A> having the Math functions
  * restricted to Expr<number>
  */
-export type Expr<A> = NumericValue<A> | Add<A> | Sub<A> | Mul<A> | Div<A>
-
-export class NumericValue<A> {
-  readonly _tag = "NumericValue"
-  constructor(readonly value: number, readonly _A: (_: number) => A) {}
-}
-
-export class Add<A> {
-  readonly _tag = "Add"
-  constructor(
-    readonly x: Expr<number>,
-    readonly y: Expr<number>,
-    readonly _A: (_: number) => A
-  ) {}
-}
-
-export class Sub<A> {
-  readonly _tag = "Sub"
-  constructor(
-    readonly x: Expr<number>,
-    readonly y: Expr<number>,
-    readonly _A: (_: number) => A
-  ) {}
-}
-
-export class Mul<A> {
-  readonly _tag = "Mul"
-  constructor(
-    readonly x: Expr<number>,
-    readonly y: Expr<number>,
-    readonly _A: (_: number) => A
-  ) {}
-}
-
-export class Div<A> {
-  readonly _tag = "Div"
-  constructor(
-    readonly x: Expr<number>,
-    readonly y: Expr<number>,
-    readonly _A: (_: number) => A
-  ) {}
-}
-
-export function value(value: number): Expr<number> {
-  return new NumericValue(value, identity)
-}
-
-export function add(y: Expr<number>): (x: Expr<number>) => Expr<number> {
-  return (x) => new Add(x, y, identity)
-}
-
-export function sub(y: Expr<number>): (x: Expr<number>) => Expr<number> {
-  return (x) => new Sub(x, y, identity)
-}
-
-export function mul(y: Expr<number>): (x: Expr<number>) => Expr<number> {
-  return (x) => new Mul(x, y, identity)
-}
-
-export function div(y: Expr<number>): (x: Expr<number>) => Expr<number> {
-  return (x) => new Div(x, y, identity)
-}
+export type Expr<A> = {}
 
 /**
  * Exercise:
