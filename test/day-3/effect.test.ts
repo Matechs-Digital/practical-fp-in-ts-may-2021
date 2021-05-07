1bb442c6c57d0efc56c921b8ba83bee72bf4af0c
Michael Arnaldi <arnaldimichael@gmail.com>
2021-05-07 10:55:24 +0100

    push

--- test/day-3/effect.test.ts
+++ test/day-3/effect.test.ts
@@ -1,4 +1,5 @@
 import * as App from "@app/exercises/day-3/01-effect"
+import { pipe } from "@effect-ts/core"
 import * as T from "@effect-ts/core/Effect"
 import * as Ex from "@effect-ts/core/Effect/Exit"
 
@@ -23,5 +24,17 @@ describe("Effect", () => {
     Ex.assertsFailure(res)
 
     expect(Ex.untraced(res)).toEqual(Ex.die("error"))
+  })
+
+  it("should access", async () => {
+    const env = pipe(App.read, T.provideAll({ input: "hey!" }))
+    const res = await T.runPromise(env)
+    expect(res).toEqual("hey!")
+  })
+
+  it("should succeedWith", async () => {
+    const res = await T.runPromiseExit(T.succeedWith(() => 1))
+
+    expect(res).toEqual(Ex.succeed(1))
   })
 })
