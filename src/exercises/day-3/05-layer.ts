462b4b26136792dedfc871e21c9ee4013e85bd3e
Michael Arnaldi <arnaldimichael@gmail.com>
2021-05-07 08:39:05 +0100

    updates

---
+++ src/exercises/day-3/05-layer.ts
@@ -0,0 +1,13 @@
+/**
+ * Exercise:
+ *
+ * Import the module `import * as L from "@effect-ts/core/Effect/Layer"` and test M.fromEffect,
+ *
+ * note: you provide a layer to an effect using T.provideSomeLayer (where T is the Effect module)
+ */
+
+/**
+ * Exercise:
+ *
+ * Inspect the module functions and operators testing behaviour 1-by-1
+ */
